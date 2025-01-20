9e99972c1d6ce95eb3aec9ffb707dbaaa626bc42
jiyuuuuun <hwangjy001@naver.com>
2025-01-20 16:08:52 +0900

    250120

---
+++ workspace/iocexam/src/main/java/annotation/Hello.java
@@ -0,0 +1,8 @@
+package annotation;
+
+public class Hello {
+    @Count100
+    public void printHello() {
+        System.out.println("Hello");
+    }
+}
---
+++ workspace/iocexam/src/main/java/annotation/HelloRun.java
@@ -0,0 +1,9 @@
+package annotation;
+
+public class HelloRun {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+
+        hello.printHello();
+    }
+}
