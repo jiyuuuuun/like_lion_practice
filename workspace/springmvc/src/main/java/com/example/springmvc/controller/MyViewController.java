be3f21180636f45a2783e4c6849f768c08ab7d1a
jiyuuuuun <hwangjy001@naver.com>
2025-02-03 16:36:33 +0900

    뷰리졸버 커스텀

---
+++ workspace/springmvc/src/main/java/com/example/springmvc/controller/MyViewController.java
@@ -0,0 +1,11 @@
+package com.example.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MyViewController {
+    @GetMapping("/custom")
+    public String customView(){
+        return "my-prefix-custom";
+    }
+}
