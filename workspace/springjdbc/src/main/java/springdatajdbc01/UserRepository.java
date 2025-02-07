a8412198ad44656c869acdbd586c7a660d68974f
jiyuuuuun <hwangjy001@naver.com>
2025-02-07 09:44:57 +0900

    UserRepository 주석 추가

--- workspace/springjdbc/src/main/java/springdatajdbc01/UserRepository.java
+++ workspace/springjdbc/src/main/java/springdatajdbc01/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@Repository // 구현체에 @Repository 붙이지만 명시적으로 표현하기 위해 붙임
 public interface UserRepository extends CrudRepository<User, Long>,CustomUserRepository {
     List<User> findByName(String name);
 }
