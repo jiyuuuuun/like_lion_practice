fa398928b34ecb16afe9bcff4bb7d7d48b34785a
jiyuuuuun <hwangjy001@naver.com>
2025-02-07 13:12:45 +0900

    friendapp 프로젝트 생성

---
+++ workspace/friendapp/src/main/java/com/example/friendapp/FriendappApplication.java
@@ -0,0 +1,13 @@
+package com.example.friendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendappApplication.class, args);
+	}
+
+}
