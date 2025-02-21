5d70328291903e72ab1bb7a47ffa1c681e71be9c
jiyuuuuun <hwangjy001@naver.com>
2025-02-21 11:14:52 +0900

    250221: Fetch Join

--- workspace/springdatajpa/src/main/java/com/example/springdatajpa4/Application.java
+++ workspace/springdatajpa/src/main/java/com/example/springdatajpa4/Application.java
@@ -1,11 +1,12 @@
 package com.example.springdatajpa4;
 
-import com.example.springdatajpa2.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @Slf4j
@@ -16,7 +17,14 @@ public class Application {
     @Bean
     public CommandLineRunner demo(EmployeeRepository employeeRepository) {
         return args -> {
-            employeeRepository.findAll().forEach(employee -> log.info(employee.toString()));
+            //employeeRepository.findAll().forEach(employee -> log.info(employee.toString()));
+
+            log.info("Employees in department 1 with salary between 2900 and 3100:");
+            employeeRepository.findBySalaryBetween(2900.0,3100.0).forEach(employee -> log.info(employee.toString()));
+
+            log.info("Employees in department 1 with salary between 2900 and 3100:      Fetch Join===================");
+            employeeRepository.findByDepartmentIdInAndSalaryBetween(Arrays.asList(30), 2900.0, 3100.0)
+                    .forEach(employee -> log.info(employee.toString()));
 
         };
     }
