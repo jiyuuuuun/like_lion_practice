8ef8bf463e92804cfbc37587b85c4a4a8b60103a
jiyuuuuun <hwangjy001@naver.com>
2025-02-27 17:13:34 +0900

    250227:

--- workspace/securityexam/src/main/java/com/example/beforesecurity/FilterConfig.java
+++ workspace/securityexam/src/main/java/com/example/beforesecurity/FilterConfig.java
@@ -1,14 +1,13 @@
 package com.example.beforesecurity;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.RegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfig { //디테일하게 등록하고 싶을 때
     @Bean
-    public FilterRegistrationBean<JiyunFilter> JiyunFilter(RegistrationBean registrationBean) {
+    public FilterRegistrationBean<JiyunFilter> JiyunFilter() {
         FilterRegistrationBean<JiyunFilter> jiyunFilterFilterRegistrationBean = new FilterRegistrationBean<>();
         jiyunFilterFilterRegistrationBean.setFilter(new JiyunFilter());
         jiyunFilterFilterRegistrationBean.addUrlPatterns("/*"); //특정한 url
