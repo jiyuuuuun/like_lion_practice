9da914297238905e10f11af089b016b7d8ca4909
jiyuuuuun <hwangjy001@naver.com>
2025-02-27 17:10:36 +0900

    250227: ThreadLocal

---
+++ workspace/securityexam/src/main/java/com/example/beforesecurity/UserContext.java
@@ -0,0 +1,18 @@
+package com.example.beforesecurity;
+
+public class UserContext {
+    private static final ThreadLocal<User> userThreadLocal = ThreadLocal.withInitial(()->null);
+
+    public static void setUser(User user) {
+        userThreadLocal.set(user);
+    }
+    public static User getUser() {
+        return userThreadLocal.get();
+    }
+    public static void removeUser() {
+        userThreadLocal.remove();
+    }
+
+
+
+}
--- workspace/securityexam/src/main/java/com/example/beforesecurity/UserFilter.java
+++ workspace/securityexam/src/main/java/com/example/beforesecurity/UserFilter.java
@@ -13,8 +13,19 @@ import java.io.IOException;
 public class UserFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        log.info("📌UserFilter doFilter 실행 전");
-        filterChain.doFilter(servletRequest, servletResponse);
-        log.info("📌UserFilter doFilter 실행 후");
+        try {
+            log.info("📌UserFilter doFilter 실행 전" + Thread.currentThread().getName());
+
+            String name = (String) servletRequest.getParameter("name");
+            // 쓰레드 로컬에 저장하고 싶은 객체가 존재 할 시
+            UserContext.setUser(new User(name));
+
+            // ✅ 필터 체인 실행 (여기서 컨트롤러, 서비스 등 실행됨)
+            filterChain.doFilter(servletRequest, servletResponse);
+            log.info("📌UserFilter doFilter 실행 후" + Thread.currentThread().getName());
+        }finally {
+            // ✅ 요청이 끝난 후 ThreadLocal에서 User 객체 삭제 (쓰레드 풀에서 재사용 해야함)
+            UserContext.removeUser();
+        }
     }
 }
